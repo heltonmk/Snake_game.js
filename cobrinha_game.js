fd25853ae4333f8d9f941551a3e5793196254865
Helton Kishi <heltonmk@gmail.com>
2011-07-18 00:05:45 -0300

    Added snake pixel size parameter.

--- cobrinha_game.js
+++ cobrinha_game.js
@@ -1,13 +1,9 @@
 function draw(elementID) {
     var canvas_elem = document.getElementById(elementID);
     var canvas_context = canvas_elem.getContext("2d");
-    //canvas_context.fillRect(0, 0, 800, 300);
-    //canvas_context.strokeRect(0, 0, 800, 300);
-    //bloco = Block();
-    //bloco.init({pos_x: 40, pos_y: 40, v_x: -5, v_y: 0});
-    
+   
     snake = Snake();
-    snake.init({pos_x: 40, pos_y: 40, v_x: 5, v_y: 0});
+    snake.init({pos_x: 40, pos_y: 40, v_x: GameParameters.block_size, v_y: 0});
     snake.eat();
     snake.eat();
     snake.eat();
@@ -19,7 +15,7 @@ function draw(elementID) {
         canvas_elem.width = canvas_elem.width;
         now = snake.iterate();
         for (var i=0; i < now.length; i++) {
-            canvas_context.fillRect(now[i].pos_x, now[i].pos_y, 5, 5);
+            canvas_context.fillRect(now[i].pos_x, now[i].pos_y, GameParameters.block_size, GameParameters.block_size);
         }
     }, 200);
 
@@ -42,60 +38,59 @@ function draw(elementID) {
                 return false;
         }
     });
-    //canvas_context = fillRect(
-    //canvas_context.lineTo(200.5, 250);
-    //canvas_context.lineTo(250, 10);
 
-    //canvas_context.strokeStyle = "#000000";
-    //canvas_context.stroke();
 }
-
+var GameParameters = {block_size:10};
 var Snake = (function() {
-    var obj = {};
-    obj.blocks = [];
-    obj.size = 1;
+    var obj = {},
+        blocks = [],
+        size = 1;
 
     obj.init = function(param) {
         var _block = Block();
         _block.init(param);
-        obj.blocks.push(_block);
+        blocks.push(_block);
     }
 
     obj.iterate = function() {
         var result = [];
 
-        for (var i=0; i < obj.blocks.length; i++) {
-            var now = obj.blocks[i].iterate();
+        for (var i=0; i < blocks.length; i++) {
+            var now = blocks[i].iterate();
             result.push(now);
         }        
         
         // Update speeds
-        for (i=obj.blocks.length-1; i > 0; i--) {
-            prev_block_speed = obj.blocks[i-1].getSpeed();
-            obj.blocks[i].setSpeed(prev_block_speed[0], prev_block_speed[1]);
+        for (i=blocks.length-1; i > 0; i--) {
+            prev_block_speed = blocks[i-1].getSpeed();
+            blocks[i].setSpeed(prev_block_speed[0], prev_block_speed[1]);
         }
         return result;
     }
 
     obj.eat = function(t) {
         var _new_block = Block();
-        var _last_block_pos = obj.blocks[obj.blocks.length - 1].getPosition();
-        _new_block.init({pos_x:_last_block_pos[0]-5, pos_y: _last_block_pos[1], v_x: 5, v_y: 0});
-        obj.size += 1;
-        obj.blocks.push(_new_block);
+        var _last_block_pos = blocks[blocks.length - 1].getPosition();
+        _new_block.init({
+                            pos_x:_last_block_pos[0]-GameParameters.block_size,
+                            pos_y: _last_block_pos[1], 
+                            v_x: GameParameters.block_size, 
+                            v_y: 0});
+        size += 1;
+        blocks.push(_new_block);
     }
 
     obj.goUp = function() {
-        obj.blocks[0].goUp();
+        blocks[0].goUp();
     }
     obj.goDown = function() {
-        obj.blocks[0].goDown();
+        blocks[0].goDown();
     }
     obj.goLeft = function() {
-        obj.blocks[0].goLeft();
+        blocks[0].goLeft();
     }
     obj.goRight = function() {
-        obj.blocks[0].goRight();
+        blocks[0].goRight();
     }
 
     return obj;
@@ -106,7 +101,7 @@ var Block = (function() {
     var obj = {},
         pos_x = 0,
         pos_y = 0,
-        v_x = 5,
+        v_x = GameParameters.block_size,
         v_y = 0;
 
     obj.init = function(param) {
@@ -137,19 +132,19 @@ var Block = (function() {
     }
 
     obj.goUp = function() {
-        v_y = -5;
+        v_y = -1 * GameParameters.block_size;
         v_x = 0;
     }
     obj.goDown = function() {
-        v_y = 5;
+        v_y = GameParameters.block_size;
         v_x = 0;
     }
     obj.goLeft = function() {
-        v_x = -5;
+        v_x = -1 * GameParameters.block_size;
         v_y = 0;
     }
     obj.goRight = function() {
-        v_x = 5;
+        v_x = GameParameters.block_size;
         v_y = 0;
     }
 
